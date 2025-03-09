f91ba907ea62d5c1649b263ceb5aa57c34af6ce7
Radford Smith <11401+rads@users.noreply.github.com>
2025-03-08 23:58:37 -0800

    debug

--- src/rpub/admin/impl.clj
+++ src/rpub/admin/impl.clj
@@ -168,7 +168,7 @@
   (for [[_ src] (set (:imports import-map))]
     [:link {:rel "modulepreload" :href src}]))
 
-(def import-script "import 'rpub.admin';")
+(def import-script "import 'preact/debug'; import 'rpub.admin';")
 
 (defn layout [{:keys [title content head import-map]}]
   (str
@@ -325,6 +325,8 @@
            "cherry-cljs/lib/clojure.string.js" "https://cdn.jsdelivr.net/npm/cherry-cljs@0.3.23/lib/clojure.string.js"
            "flowbite" "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"
            "preact" "https://cdn.jsdelivr.net/npm/preact@10.25.0/dist/preact.module.js"
+           "preact/debug" "https://cdn.jsdelivr.net/npm/preact@10.25.0/debug/dist/debug.module.js"
+           "preact/devtools" "https://cdn.jsdelivr.net/npm/preact@10.25.0/devtools/dist/devtools.module.js"
            "preact/compat" "https://cdn.jsdelivr.net/npm/preact@10.25.0/compat/dist/compat.module.js"
            "preact/hooks" "https://cdn.jsdelivr.net/npm/preact@10.25.0/hooks/dist/hooks.module.js"
            "react" "https://cdn.jsdelivr.net/npm/preact@10.25.0/compat/dist/compat.module.js"
