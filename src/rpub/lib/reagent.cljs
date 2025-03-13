4977168d8fb00d0add719f67579fb8a71901874e
Radford Smith <11401+rads@users.noreply.github.com>
2025-03-13 12:08:56 -0700

    Support hiccup css shortcuts

--- src/rpub/lib/reagent.cljs
+++ src/rpub/lib/reagent.cljs
@@ -1,11 +1,25 @@
 (ns rpub.lib.reagent
   {:no-doc true}
-  (:require ["react" :as react]))
+  (:require ["react" :as react]
+            [clojure.string :as str]))
+
+(defn- add-classes [attrs classes]
+  (let [classes' (if (:class attrs)
+                   (conj classes (:class attrs))
+                   classes)]
+    (str/join " " classes')))
 
 (defn- parse-element [form]
-  (if (map? (second form))
-    {:el (first form), :attrs (second form), :children (drop 2 form)}
-    {:el (first form), :children (rest form)}))
+  (let [parsed (if (map? (second form))
+                 (let [[el attrs & children] form]
+                   {:el el, :attrs attrs, :children children})
+                 (let [[el & children] form]
+                   {:el el, :children children}))]
+    (if-not (keyword? (:el parsed))
+      parsed
+      (let [[el' & classes] (str/split (name (:el parsed)) #"\.")]
+        (cond-> (assoc parsed :el el')
+          (seq classes) (update-in [:attrs :class] add-classes classes))))))
 
 (defn- ->clj-props [js-props]
   (reduce (fn [m [k v]] (assoc m (keyword k) v))
@@ -18,33 +32,37 @@
   (let [clj-props (-> (->clj-props js-props) (dissoc :__el))]
     (as-element ((.-__el js-props) clj-props))))
 
+(defn- string->el [s] s)
+
+(defn- map->el [m]
+  (reduce (fn [acc [k v]]
+            (aset acc (name k) v)
+            acc)
+          (js-obj)
+          m))
+
+(defn- keyword-vector->el [form]
+  (let [{:keys [el attrs children]} (parse-element form)
+        children' (as-element children)
+        attrs' (some-> attrs as-element)]
+    (react/createElement el attrs' children')))
+
+(defn- fn-vector->el [form]
+  (let [{:keys [el attrs children]} (parse-element form)
+        children' (as-element children)
+        attrs' (as-element (assoc attrs :__el el))]
+    (react/createElement Adapter attrs' children')))
+
+(defn- sequential->el [xs]
+  (into-array (map as-element xs)))
+
 (defn as-element [form]
   (cond
-    (string? form) form
-
-    (and (map? form) (not (record? form)))
-    (reduce (fn [acc [k v]]
-              (aset acc (name k) v)
-              acc)
-            (js-obj)
-            form)
-
-    (vector? form)
-    (let [{:keys [el attrs children]} (parse-element form)
-          children' (into-array (map as-element children))]
-      (cond
-        (keyword? el) (react/createElement
-                        (name el)
-                        (some-> attrs as-element)
-                        children')
-        (fn? el) (react/createElement
-                   Adapter
-                   (as-element (assoc attrs :__el el))
-                   children')))
-
-    (sequential? form)
-    (into-array (map as-element form))
-
+    (string? form) (string->el form)
+    (and (map? form) (not (record? form))) (map->el form)
+    (and (vector? form) (keyword? (first form))) (keyword-vector->el form)
+    (and (vector? form) (fn? (first form))) (fn-vector->el form)
+    (sequential? form) (sequential->el form)
     :else form))
 
 (defn reactify-component [reagent-component]
