596e6a2f11dd5e369c16ab8c89117a039c9ad377
Radford Smith <11401+rads@users.noreply.github.com>
2025-04-10 19:06:39 -0700

    Fix login page

--- src/rpub/admin.clj
+++ src/rpub/admin.clj
@@ -154,17 +154,15 @@
         (admin-impl/logo {:class "mb-8 text-6xl"})
         [:div {:class "w-full bg-white rounded-lg shadow-sm md:mt-0 max-w-md xl:p-0"}
          [:div {:class "p-6 space-y-4 md:space-y-6 sm:p-8"}
-          [:form
-           {:class "space-y-4 md:space-y-6"}
-           {:action "#"}
+          [:form {:class "space-y-4 md:space-y-6" :action "#"}
            [:div
-            [:input#username {:class "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"}
-             {:type :text :name :username :placeholder "Username" :required ""}]]
+            [:input#username {:class "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                              :type :text :name :username :placeholder "Username" :required ""}]]
            [:div
-            [:input#password {:class "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"}
-             {:type :password :name :password :placeholder "Password" :required ""}]]
-           [:button {:class "w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"}
-            {:type "submit"}
+            [:input#password {:class "bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                              :type :password :name :password :placeholder "Password" :required ""}]]
+           [:button {:class "w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                     :type "submit"}
             "Sign In"]]]]])}))
 
 (defn- next-url [req]
